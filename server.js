defbc1e650ec799079eaf138dd48ac941c49e339
loreeue <loretouzquiano@gmail.com>
2026-08-06 19:30:47 +0200

    update

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const indexHtml = fs.readFileSync(path.join(__dirname, 'index.html'));
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('¡Despliegue completado!\n');
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.end(indexHtml);
 });
+
 server.listen(3000, () => console.log('Servidor en puerto 3000'));
